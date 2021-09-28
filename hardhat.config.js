4666bdec55c842a1f675a5e900d911b21eed2856
trieult <amhanhnguxu@gmail.com>
2021-09-28 22:09:19 +0700

    update

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,14 +8,14 @@ module.exports = {
     rinkeby: {
       url: `https://rinkeby.infura.io/v3/e8546a58c32d472790076824beaeae62`,
       accounts: {
-        mnemonic: 'promote involve unaware today camera major net tail area rule manual humor'
-        // mnemonic: 'into exclude when nest zoo render draw spoil winner agree blind canvas'
+        mnemonic: '****'
+        // mnemonic: '***'
       }
     },
     goeri: {
       url: `https://goeri.infura.io/v3/e8546a58c32d472790076824beaeae62`,
       accounts: {
-        mnemonic: 'promote involve unaware today camera major net tail area rule manual humor'
+        mnemonic: '****'
       }
     },
     bsc: {
@@ -23,7 +23,7 @@ module.exports = {
       chainId: 97,
       gasPrice: 20000000000,
       accounts: {
-        mnemonic: 'promote involve unaware today camera major net tail area rule manual humor'
+        mnemonic: '****'
       }
     }
   }
