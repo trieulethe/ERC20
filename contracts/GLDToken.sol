a0702ddf8209f025c10246a8d30023c471fb1f71
trieult <trieult@icheck.vn>
2021-09-28 10:51:42 +0700

    update

--- contracts/GLDToken.sol
+++ contracts/GLDToken.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
